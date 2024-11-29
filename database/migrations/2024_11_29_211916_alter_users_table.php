77a65dabd0f672b6a45d703e0f3c9cfc7ffe527b
anvillegasm <avillegasmur23@gmail.com>
2024-11-29 17:30:03 -0600

    Migración de datos
    Se hace una migración a la tabla de usuarios para colocar nuevos atributos

--- database/migrations/2024_11_29_211916_alter_users_table.php
+++ database/migrations/2024_11_29_211916_alter_users_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use function Laravel\Prompts\table;
-
 return new class extends Migration
 {
-    
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
@@ -20,7 +20,9 @@ return new class extends Migration
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
