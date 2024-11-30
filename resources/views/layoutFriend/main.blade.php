059f064e9886287462334ce384cec1e844a7ea87
Paulo22mc <juanpaulomejias@outlook.com>
2024-11-29 19:10:05 -0600

    Archivo main de amigos
    Este es el archivo html principal de la vista de amigos

---
+++ resources/views/layoutFriend/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Trees 2</title>
+    <link rel="stylesheet" href=" {{ asset('css/friend/dashboard.css') }}">
+    <link rel="stylesheet" href=" {{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('images/icon.png') }}" type="image/png">
+</head>
+<body>
+    @include('layoutFriend.navbar')
+
+    
+</body>
+</html>
