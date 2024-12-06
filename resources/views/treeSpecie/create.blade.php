d05840d21417a2a13856a90bc29ebe8536e057db
anvillegasm <avillegasmur23@gmail.com>
2024-12-05 19:56:36 -0600

    Creación vista de creación de especies
    Se creó la vista para que el usuario administrador puede ingresar los datos de una nueva especie

---
+++ resources/views/treeSpecie/create.blade.php
@@ -0,0 +1,25 @@
+@extends('treeSpecie.main')
+
+@section('content')
+    <div class="container">
+        <div class="form-container">
+            <h2>Register New Tree Species</h2>
+
+            <form action="{{ route('treeSpecie.save') }}" method="POST">
+                @csrf
+
+                <div class="input-contenedor">
+                    <label for="comercialName">Comercial Name</label>
+                    <input type="text" name="comercialName" id="comercialName" required>
+                </div>
+
+                <div class="input-contenedor">
+                    <label for="scientificName">Scientific Name</label>
+                    <input type="text" name="scientificName" id="scientificName" required>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+@endsection
