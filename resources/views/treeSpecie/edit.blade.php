7f64459bf8b56f4eaff45a8da279721d2bc276de
Paulo22mc <juanpaulomejias@outlook.com>
2024-12-05 21:38:34 -0600

    Corrección en form de edit
    Se cambia la mayoría de la estructura del formulario con diferentes clases para el estilo.

--- resources/views/treeSpecie/edit.blade.php
+++ resources/views/treeSpecie/edit.blade.php
@@ -1,16 +1,31 @@
-<form action="{{ route('treeSpecie.update', ['id' => $tree->id]) }}" method="POST">
-    @csrf
-    @method('PUT') 
+@extends('treeSpecie.main') 
 
-    <div class="input-contenedor">
-        <label for="comercialName">Comercial Name</label>
-        <input type="text" name="comercialName" id="comercialName" value="{{ old('comercialName', $tree->comercialName) }}" required>
+@section('content')
+<div class="edit-container">
+    <div class="form-container">
+        <h2 class="edit-title">Edit Tree Species</h2>
+        <form action="{{ route('treeSpecie.update', $tree->id) }}" method="POST" class="edit-form">
+            @csrf
+            @method('PUT')
+
+            <div class="input-contenedor">
+                <label for="comercialName" class="form-label">Commercial Name</label>
+                <input type="text" name="comercialName" id="comercialName" 
+                       class="form-input" value="{{ $tree->comercialName }}" required>
+            </div>
+
+            <div class="input-contenedor">
+                <label for="scientificName" class="form-label">Scientific Name</label>
+                <input type="text" name="scientificName" id="scientificName" 
+                       class="form-input" value="{{ $tree->scientificName }}" required>
+            </div>
+
+            <div class="input-contenedor">
+                <button type="submit" class="btn btn-primary">Update</button>
+                <a href="{{ route('treeSpecie.show') }}" class="btn btn-secondary">Cancel</a>
+            </div>
+        </form>
     </div>
+</div>
+@endsection
 
-    <div class="input-contenedor">
-        <label for="scientificName">Scientific Name</label>
-        <input type="text" name="scientificName" id="scientificName" value="{{ old('scientificName', $tree->scientificName) }}" required>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Save</button>
-</form>
