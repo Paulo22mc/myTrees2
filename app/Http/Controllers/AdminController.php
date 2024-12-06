7a24cb2b0f94a44c4b7fefbd65f13503a8d44e60
anvillegasm <avillegasmur23@gmail.com>
2024-12-05 19:46:01 -0600

    Edición del controller del admin
    Se colocaron datos quemados para probar vista

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,6 +10,11 @@ class AdminController extends Controller
 
     public function showMainView()
     {
-        return view('layoutAdmin.main');
+       
+        $friends = 10; 
+        $availableTrees = 20; 
+        $treeSold = 15; 
+
+        return view('layoutAdmin.main', compact('friends', 'availableTrees', 'treeSold'));
     }
 }
