e436cc6653eca87da00f93593cefbca67eb1a960
Paulo22mc <juanpaulomejias@outlook.com>
2024-12-06 16:50:39 -0600

    Nuevas funciones en AddUserControler
    Se crearon funciones para editar la informacion del usuario admin u operador.
    Se crea una funcion para mostrar una tabla con los usuarios admin y operadores.
    Se crea la funcion para eliminar usuarios.

--- app/Http/Controllers/AddUsersController.php
+++ app/Http/Controllers/AddUsersController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\AddUsers;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AddUsersController extends Controller
 {
     public function register(Request $request)
     {
-        
+
         $request->validate([
             'name' => 'required|string|max:255',
             'lastName' => 'required|string|max:255',
@@ -39,5 +40,45 @@ class AddUsersController extends Controller
     public function addUsers()
     {
         return view('AddUsers.main');
+    }
+
+    public function show()
+    {
+        // Obtener usuarios con roles 'operator' y 'administrator'
+        $users = User::whereIn('role', ['operator', 'administrator'])->get();
+
+        return view('AddUsers.show', compact('users'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('AddUsers.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'lastName' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'number' => 'required|string|max:15',
+            'address' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'role' => 'required|in:administrator,operator',
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->update($validatedData);
+
+        return redirect('/users')->with('success', 'User updated successfully.');
+    }
+
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->back()->with('success', 'User deleted successfully');
     }
 }
