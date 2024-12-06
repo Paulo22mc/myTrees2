4c98e64ae1614d2a9ea85f17e3d578e254bb64f4
anvillegasm <avillegasmur23@gmail.com>
2024-12-06 15:41:29 -0600

    Cambio titulo
    Se cambió el titulo de la página

--- resources/views/treeForSale/main.blade.php
+++ resources/views/treeForSale/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tree Species </title>
+    <title>Tree For Sale </title>
     <link rel="stylesheet" href=" {{ asset('css/style.css') }}">
     <link rel="icon" href="{{ asset('images/icon.png') }}" type="image/png">
     <link rel="stylesheet" href=" {{ asset('css/admin/treeSpecies.css') }}">
