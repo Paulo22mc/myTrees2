47a2954c1974a8a5f7610c3d9f932c11d9b48d43
anvillegasm <avillegasmur23@gmail.com>
2024-12-05 19:59:26 -0600

    Vista updated especies
    Se creó una vista para el updated de las especies

---
+++ resources/views/treeSpecie/updated.blade.php
@@ -0,0 +1,26 @@
+@extends('treeSpecie.main')
+
+@section('content')
+    <div class="container">
+        <div class="form-container">
+            <h2>Edit Tree Species</h2>
+
+            <form action="{{ route('treeSpecie.edit', ['id' => $tree->id]) }}" method="POST">
+                @csrf
+                @method('PUT') 
+
+                <div class="input-contenedor">
+                    <label for="comercialName">Comercial Name</label>
+                    <input type="text" name="comercialName" id="comercialName" value="{{ old('comercialName', $tree->comercialName) }}" required>
+                </div>
+
+                <div class="input-contenedor">
+                    <label for="scientificName">Scientific Name</label>
+                    <input type="text" name="scientificName" id="scientificName" value="{{ old('scientificName', $tree->scientificName) }}" required>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+@endsection
