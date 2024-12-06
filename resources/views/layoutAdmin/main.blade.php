50fa9e03b6e218cf91b5c806d535f1343a4152e9
anvillegasm <avillegasmur23@gmail.com>
2024-12-05 19:55:11 -0600

    Creación vista admin
    Esta es la vista principal del administrador

--- resources/views/layoutAdmin/main.blade.php
+++ resources/views/layoutAdmin/main.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Dashboard Administrator</title>
+    <link rel="stylesheet" href=" {{ asset('css/admin/dashboard.css') }}">
+    <link rel="stylesheet" href=" {{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('images/icon.png') }}" type="image/png">
 </head>
 <body>
-    <h1>Hola admin</h1>
+    @include('layoutAdmin.navbar')
+
+    
 </body>
 </html>
