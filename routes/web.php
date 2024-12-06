1000db089b075e9c11a7d121d71ce831e46d0ad2
anvillegasm <avillegasmur23@gmail.com>
2024-12-05 19:59:50 -0600

    Nuevas rutas
    Se añadieron nuevas rutas para crear, editar y eleiminar especies de árboles

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\FriendController;
+use App\Http\Controllers\TreeSpeciesController;
+
 
 // Ruta para mostrar el formulario de login
 Route::get('/', [AuthenticationController::class, 'showLoginForm'])->name('login');
@@ -29,3 +31,18 @@ Route::get('/admin/dashboard', [AdminController::class, 'showMainView'])->name('
 //Route::get('/operator/dashboard', [OperatorController::class, 'index'])->name('operator.dashboard');
 Route::get('/friend/dashboard', [FriendController::class, 'showMainView'])->name('friend.dashboard');
 
+
+// Mostrar el formulario para registrar nuevas especies
+Route::get('/treeSpecie/create', [TreeSpeciesController::class, 'create'])->name('treeSpecie.create');
+
+// Registrar nueva especie de árbol
+Route::post('/treeSpecie', [TreeSpeciesController::class, 'save'])->name('treeSpecie.save');
+
+// Mostrar todas las especies registradas
+Route::get('/treeSpecie/show', [TreeSpeciesController::class, 'show'])->name('treeSpecie.show');
+
+// Ruta para procesar la actualización de una especie
+Route::put('/treeSpecie/updated/{id}', [TreeSpeciesController::class, 'updated'])->name('treeSpecie.updated');
+
+// Eliminar todas las especies registradas
+Route::delete('/treeSpecie/{id}', [TreeSpeciesController::class, 'destroy'])->name('treeSpecie.destroy');
