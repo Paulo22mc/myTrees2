6944af1393bd24583b9920df58c8e8069f82e7ae
anvillegasm <avillegasmur23@gmail.com>
2024-12-07 10:01:01 -0600

    Modificación buyForm
    Se hizo el modelo del buyForm

--- app/Models/BuyForm.php
+++ app/Models/BuyForm.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BuyForm extends Model
 {
-    //
+   use HasFactory;
+
+   protected $table = 'tree';
+   protected $fillable = [
+      'idSpecie',
+      'idFriend',
+      'ubication',
+      'status',
+      'size',
+      'price',
+      'photo'
+   ];
+
+   public function specie()
+   {
+       return $this->belongsTo(TreeSpecies::class, 'idSpecie');
+   }
+
+   public function friend()
+   {
+       return $this->belongsTo(User::class, 'idFriend');
+   }
 }
