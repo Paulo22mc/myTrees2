6854476b41a32f4bf53d0c29ea3171592ff45a73
Paulo22mc <juanpaulomejias@outlook.com>
2024-12-08 16:18:34 -0600

    Ventana de Detalles de amigo
    En esta parte el admin puede ver una tabla con los arboles comprados del amigo

---
+++ resources/views/friends/friendDetails.blade.php
@@ -0,0 +1,38 @@
+@extends('friends.app')
+
+@section('content')
+    <h2>Details of Friend: {{ $friend->name }}</h2>
+    <div>
+        <h3>Trees Registered</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Commercial Name</th>
+                    <th>Size</th>
+                    <th>Ubication</th>
+                    <th>Photo</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trees as $tree)
+                    <tr>
+                        <td>{{ $tree->comercialName }}</td>
+                        <td>{{ $tree->size }}</td>
+                        <td>{{ $tree->ubication }}</td>
+                        <td>
+                            @if($tree->photo)
+                                <a href="{{ asset('storage/' . $tree->photo) }}" target="_blank">View Photo</a>
+                            @else
+                                No Photo
+                            @endif
+                        </td>
+                        <td>
+                            <a href="{{ route('friends.edit', ['id' => $tree->id]) }}" class="action-button">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
