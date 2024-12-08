bbb53e96a5795482d320d71699a0f2568d76e628
anvillegasm <avillegasmur23@gmail.com>
2024-12-07 23:52:48 -0600

    Nueva Vista
    Se creó una vista para añadir todos los datos de los árboles por cada amigo

--- resources/views/seeMyTrees/main.blade.php
+++ resources/views/seeMyTrees/main.blade.php
@@ -1,28 +1,46 @@
-<div class="treeTableContainer">
-    <table class="treeTable">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Trees</title>
+    <link rel="stylesheet" href="{{ asset('css/friend/seeMyTrees.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+
+    @include('layoutadmin.navbar')
+
+    <h2>All My Trees</h2>
+
+    <table>
         <thead>
             <tr>
-                <th>Commercial Name</th>
-                <th>Price</th>
-                <th>Size</th>
                 <th>Photo</th>
-                <th>Ubication</th>
+                <th>Specie</th>
+                <th>Location</th>
+                <th>Status</th>
+                <th>Size</th>
+                <th>Price</th>
             </tr>
         </thead>
         <tbody>
-            @forelse ($trees as $tree)
+            @foreach ($trees as $tree)
                 <tr>
-                    <td>{{ $tree->commercialName }}</td>  <!-- Nombre comercial de la especie -->
-                    <td>${{ $tree->price }}</td>  <!-- Precio -->
-                    <td>{{ $tree->size }} cm</td>  <!-- Tamaño -->
-                    <td><img src="{{ asset('images/trees/' . $tree->photo) }}" alt="{{ $tree->name }}" width="50"></td>  <!-- Foto -->
-                    <td>{{ $tree->ubication }}</td>  <!-- Ubicación -->
+                    <td><img src="{{ asset('storage/' . $tree->photo) }}" alt="Tree photo" width="100"></td>
+                    <td>{{ $tree->species->comercialName }}</td>
+                    <td>{{ $tree->ubication }}</td>
+                    <td>{{ $tree->status }}</td>
+                    <td>{{ $tree->size }}</td>
+                    <td>${{ number_format($tree->price, 2) }}</td>
                 </tr>
-            @empty
-                <tr>
-                    <td colspan="8">No trees found.</td>  <!-- Mensaje si no hay árboles -->
-                </tr>
-            @endforelse
+            @endforeach
         </tbody>
     </table>
-</div>
+
+
+</body>
+
+</html>
