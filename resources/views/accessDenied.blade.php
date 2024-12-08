b3bf9dcd82592137da5b686751562a4eb71a1424
anvillegasm <avillegasmur23@gmail.com>
2024-12-08 17:26:35 -0600

    Vista Acceso denegado
    Nueva vista para cuando un rol no tiene permiso de acceder a alguna ruta que no le pertenece

---
+++ resources/views/accessDenied.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Access Denied</title>
+</head>
+<body>
+    <h2>Acceso Denegado</h2>
+    <p>Access to this page is prohibited, a specific role is required.</p>
+</body>
+</html>
