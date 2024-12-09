a3d4e5abd83607457bedda1acff3ed5460c87099
Paulo22mc <juanpaulomejias@outlook.com>
2024-12-09 12:38:11 -0600

    Ajustes en la tabla de Ver
    Cambios en la estructura del archivo donde se actualiza la información del arbol.
    Se eliminan comentarios y se agrega un enlace para volver a la parte principal.

--- resources/views/updates/update.blade.php
+++ resources/views/updates/update.blade.php
@@ -4,33 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actualizar Árbol</title>
+    <title>My Trees 2</title>
+    <link rel="stylesheet" href=" {{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('images/icon.png') }}" type="image/png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
 </head>
 
 <body>
     @include('layoutAdmin.navbar')
     <div class="container">
-        <h2>Actualizar Árbol </h2>
+        <h2>Update Tree </h2>
 
         <form action="{{ route('TreeUpdates.save') }}" method="POST" enctype="multipart/form-data">
             @csrf
-            <!-- ID del árbol -->
+            
             <input type="hidden" name="idTree" value="{{ $tree->id }}">
 
-            <!-- Campo para el tamaño -->
+            
             <div class="form-group">
                 <label for="size">Size</label>
                 <input type="number" name="size" id="size" class="form-control" required min="1"
                     step="0.01">
             </div>
 
-            <!-- Campo para la foto -->
             <div class="form-group">
                 <label for="photo">Photo</label>
                 <input type="file" name="photo" id="photo" class="form-control" accept="image/*" required>
             </div>
 
-            <!-- Botón de envío -->
+            
             <button type="submit" class="btn btn-success">Save Update</button>
 
 
@@ -45,11 +47,9 @@
             @endif
 
         </form>
+        <a href="{{route ('updates.main')}}">Cancel</a>
     </div>
 
-    <!-- Incluye tus archivos JS o el enlace a Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
